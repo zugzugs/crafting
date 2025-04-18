81ecf5dbc59aca538ad708fef162ddc85be78f2e
zugzugs <1337@hojlund.info>
2025-04-19 00:01:05 +0200

    Update scrape_wowhead.py

--- scrape_wowhead.py
+++ scrape_wowhead.py
@@ -4,6 +4,20 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
+def scroll_to_bottom(driver, pause_time=2):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(pause_time)
+
+        # Calculate new scroll height and compare with last
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break  # We're at the bottom
+        last_height = new_height
+
 def main():
     url = "https://www.wowhead.com/items"
 
@@ -14,6 +28,7 @@ def main():
 
     driver = webdriver.Chrome(options=options)
     driver.get(url)
+    scroll_to_bottom(driver, pause_time=10)
 
     time.sleep(10)
 
