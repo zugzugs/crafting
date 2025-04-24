7f55a3e0f5d54c500047fab4b8341b44a7230c90
zugzugs <1337@hojlund.info>
2025-04-24 02:43:48 +0200

    opts.add_agrs_headless=new

--- scrape_wowhead.py
+++ scrape_wowhead.py
@@ -9,7 +9,7 @@ import time
 
 def get_materials_data(url):
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
